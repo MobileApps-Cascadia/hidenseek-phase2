966baa4f56287a418bee41c5d6296cdb4e085edc
Todd123178 <todd.taylor.tt@gmail.com>
2014-12-03 13:39:45 -0800

    newest need to fix timer and add functionality to buttons on found dialog

--- HideNSeek/src/com/cascadia/hidenseek/Active.java
+++ HideNSeek/src/com/cascadia/hidenseek/Active.java
@@ -7,6 +7,7 @@ import com.cascadia.hidenseek.Player.Status;
 import com.cascadia.hidenseek.network.DeletePlayingRequest;
 import com.cascadia.hidenseek.network.GetPlayerListRequest;
 import com.cascadia.hidenseek.network.PutGpsRequest;
+import com.cascadia.hidenseek.network.PutStatusRequest;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -138,8 +139,8 @@ public class Active extends FragmentActivity   {
 									public void onClick(DialogInterface dialog,int id) {
 										// if this button is clicked, just close
 										// the dialog box and do nothing
-										dialog.cancel();
-									}
+										
+								}
 								});
 								
 				 
@@ -193,20 +194,20 @@ public class Active extends FragmentActivity   {
             // time at which alarm will be scheduled here alarm is scheduled at 1 day from current time, 
             // we fetch  the current time in milliseconds and added 1 day time
             // i.e. 24*60*60*1000= 86,400,000   milliseconds in a day        
-            Long time =  new GregorianCalendar().getTimeInMillis()+Long.parseLong(Timer)*1000;
+         //   Long time =  new GregorianCalendar().getTimeInMillis()+Long.parseLong(Timer)*1000;
 
             // create an Intent and set the class which will execute when Alarm triggers, here we have
             // given AlarmReciever in the Intent, the onRecieve() method of this class will execute when
             // alarm triggers and 
             //we will write the code to send SMS inside onRecieve() method pf Alarmreciever class
-            Intent intentAlarm = new Intent(this, AlarmReciever.class);
+          //  Intent intentAlarm = new Intent(this, AlarmReciever.class);
        
             // create the object
-            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+          //  AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
             //set the alarm for particular time
-            alarmManager.set(AlarmManager.RTC_WAKEUP,time, PendingIntent.getBroadcast(this,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
-            Toast.makeText(this, "Alarm Scheduled", Toast.LENGTH_LONG).show();
+         //   alarmManager.set(AlarmManager.RTC_WAKEUP,time, PendingIntent.getBroadcast(this,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
+        //    Toast.makeText(this, "Alarm Scheduled", Toast.LENGTH_LONG).show();
          
     }
 
