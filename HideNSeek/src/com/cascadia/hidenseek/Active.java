c9f1a4476cc7679647d9d5d9cbb5dba6d35db12a
Todd123178 <todd.taylor.tt@gmail.com>
2014-12-03 20:31:19 -0800

    So i got the found portion working.  If a seeker is to find a player, the seeker is pinged on the map.  meaning he will show up for 30 seconds, then he will disappear again.  ; ).  and also fixed the game timer to end at the right time.  now we just need to add anymore little tweaks to it and test it!!!

--- HideNSeek/src/com/cascadia/hidenseek/Active.java
+++ HideNSeek/src/com/cascadia/hidenseek/Active.java
@@ -4,9 +4,11 @@ import java.util.GregorianCalendar;
 
 import com.cascadia.hidenseek.Player.Role;
 import com.cascadia.hidenseek.Player.Status;
+import com.cascadia.hidenseek.SplashActivity.MyCountDownTimer;
 import com.cascadia.hidenseek.network.DeletePlayingRequest;
 import com.cascadia.hidenseek.network.GetPlayerListRequest;
 import com.cascadia.hidenseek.network.PutGpsRequest;
+import com.cascadia.hidenseek.network.PutRoleRequest;
 import com.cascadia.hidenseek.network.PutStatusRequest;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -31,7 +33,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -51,7 +55,11 @@ public class Active extends FragmentActivity   {
 	String Timer;
 	final Context context = this;
 	boolean tagged=true;
-			
+	private ShowHider sh;
+	Long showTime= (long) 30000;
+	
+	
+	
 	
 	//Used for periodic callback.
     private Handler h2 = new Handler();
@@ -119,6 +127,9 @@ public class Active extends FragmentActivity   {
 						googleMap.clear();
 							for (final Player p : match.players) {
 							pend = p.GetStatus();
+							if(p.GetRole()==Player.Role.Seeker){
+								temp=p;
+							}
 							playerRole = p.GetRole();
 							if (pend == Status.Spotted) {
 								if(tagged)
@@ -145,6 +156,9 @@ public class Active extends FragmentActivity   {
 										};
 										pp.DoRequest(p);
 										tagged=true;
+										ShowSeeker();
+										
+										
 										
 									
 										
@@ -167,6 +181,7 @@ public class Active extends FragmentActivity   {
 										};
 										pp.DoRequest(p);
 										tagged=true;
+										
 										
 										
 								}
@@ -236,22 +251,33 @@ public class Active extends FragmentActivity   {
             // time at which alarm will be scheduled here alarm is scheduled at 1 day from current time, 
             // we fetch  the current time in milliseconds and added 1 day time
             // i.e. 24*60*60*1000= 86,400,000   milliseconds in a day        
-         //   Long time =  new GregorianCalendar().getTimeInMillis()+Long.parseLong(Timer)*1000;
+           Long time =  new GregorianCalendar().getTimeInMillis()+Long.parseLong(Timer)*1000;
 
             // create an Intent and set the class which will execute when Alarm triggers, here we have
             // given AlarmReciever in the Intent, the onRecieve() method of this class will execute when
             // alarm triggers and 
             //we will write the code to send SMS inside onRecieve() method pf Alarmreciever class
-          //  Intent intentAlarm = new Intent(this, AlarmReciever.class);
+           Intent intentAlarm = new Intent(this, AlarmReciever.class);
        
             // create the object
-          //  AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+           AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
             //set the alarm for particular time
-         //   alarmManager.set(AlarmManager.RTC_WAKEUP,time, PendingIntent.getBroadcast(this,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
-        //    Toast.makeText(this, "Alarm Scheduled", Toast.LENGTH_LONG).show();
+           alarmManager.set(AlarmManager.RTC_WAKEUP,time, PendingIntent.getBroadcast(this,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
+           Toast.makeText(this, "Alarm Scheduled", Toast.LENGTH_LONG).show();
          
     }
+	public void ShowSeeker()
+	{ 
+		 sh=new ShowHider(showTime,1000,temp);
+		 sh.startCountDown1();
+		
+		
+		
+		
+	}
+	
+	
 
 	
 	public void onPause(){
