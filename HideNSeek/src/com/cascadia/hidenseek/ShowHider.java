bb39e66f34d895c921ab019d12b5fdd929f9b2b2
Todd123178 <todd.taylor.tt@gmail.com>
2014-12-03 22:23:45 -0800

    forgot to commit the class that shows the hider for a couple seconds, also the putstopquest for the network which looks like it just need to be written it does put the request.

---
+++ HideNSeek/src/com/cascadia/hidenseek/ShowHider.java
@@ -0,0 +1,66 @@
+package com.cascadia.hidenseek;
+
+import android.os.CountDownTimer;
+
+import com.cascadia.hidenseek.Player.Role;
+import com.cascadia.hidenseek.network.PutRoleRequest;
+
+public class ShowHider extends CountDownTimer {
+	private boolean isrunning = false;
+	Player isSeeker;
+
+	public ShowHider(long startTime, long interval) {
+			super(startTime, interval);
+		
+	}
+	public ShowHider(long StartTime,long interval, Player p){
+		super(StartTime, interval);
+		isSeeker=p;
+	}
+	
+
+	
+
+	public void startCountDown1() {
+		isrunning = true;
+		isSeeker.SetRole(Role.Supervisor);
+	    PutRoleRequest pp = new PutRoleRequest() {
+			
+			@Override
+			protected void onException(Exception e) {
+				e.printStackTrace();
+			}
+			
+								
+		};
+		pp.DoRequest(isSeeker);
+		start();
+	}
+	
+	@Override
+	public void onFinish() {
+		isrunning = true;
+		isSeeker.SetRole(Role.Seeker);
+	    PutRoleRequest pp = new PutRoleRequest() {
+			
+			@Override
+			protected void onException(Exception e) {
+				e.printStackTrace();
+			}
+			
+								
+		};
+		pp.DoRequest(isSeeker);
+		start();
+		isrunning = false;
+	}
+
+	@Override
+	public void onTick(long millisUntilFinished) {
+		
+	}
+
+	public boolean isRunning() {
+		return isrunning;
+	}
+}
---
+++ HideNSeek/src/com/cascadia/hidenseek/network/PutStopRequest.java
@@ -0,0 +1,31 @@
+package com.cascadia.hidenseek.network;
+
+import com.cascadia.hidenseek.Match;
+import com.cascadia.hidenseek.network.NetworkBase.RequestType;
+
+public class PutStopRequest extends NetworkRequest{
+	public void DoRequest(Match toStop) {
+		m = toStop;
+		Request r = new Request();
+		r.url = baseUrl + "matches/" + toStop.GetId() + "/stop/";
+		r.type = RequestType.PUT;
+		r.jsonArgs = m.ToJSONStart();
+		doRequest(r);
+	}
+	protected void onComplete(Match match) { }
+	@Override
+	protected void onException(Exception e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void processPostExecute(String s) {
+		// TODO Auto-generated method stub
+		m.stopMatch();
+		onComplete(m);
+		
+	}
+	private Match m;
+
+}
